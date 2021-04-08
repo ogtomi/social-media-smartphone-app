7d2f7fbbe02bb8edea8b36e52d339bb2703cd724
Tomek Lewandowski <tomeklewandowski570@gmail.com>
2021-04-08 17:36:46 +0200

    Added user profile tabs (posts, profile, friends)

---
+++ react-native-app/src/components/User/Friends.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from '../Themed'
+
+export default class Friends extends React.Component {
+    render() {
+        return(
+            <Text>Whatever friends</Text>
+        )
+    }
+}
