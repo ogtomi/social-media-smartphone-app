6f6c84acf8d8cdf23ab70fd4ebe8ba11e663373a
Tomek Lewandowski <tomeklewandowski570@gmail.com>
2021-10-29 11:56:13 +0200

    looks of register form improved

--- react-native-app/src/screens/Register.form.tsx
+++ react-native-app/src/screens/Register.form.tsx
@@ -1,48 +1,52 @@
 import React from "react";
-import { StyleSheet, Button, TextInput, View, Text } from "react-native";
+import { StyleSheet, View, TextInput, Text } from "react-native";
+import { Button } from "react-native-paper";
 import { Formik } from "formik";
 import { register } from "../auth/auth.api";
 
 const RegisterForm = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <Text style={styles.logo}>REGISTER</Text>
       <Formik
         initialValues={{ name: "", surname: "", email: "", password: "" }}
         onSubmit={(values) => {
-          register(values)
+          register(values);
         }}
       >
         {(formikprops) => (
           <View>
             <TextInput
-              style={styles.input}
+              style={styles.textInput}
               placeholder="Name"
               onChangeText={formikprops.handleChange("name")}
               value={formikprops.values.name}
             />
             <TextInput
-              style={styles.input}
+              style={styles.textInput}
               placeholder="Surname"
               onChangeText={formikprops.handleChange("surname")}
               value={formikprops.values.surname}
             />
             <TextInput
-              style={styles.input}
+              style={styles.textInput}
               placeholder="Email"
               onChangeText={formikprops.handleChange("email")}
               value={formikprops.values.email}
             />
             <TextInput
-              style={styles.input}
+              style={styles.textInput}
               placeholder="Password"
               onChangeText={formikprops.handleChange("password")}
               value={formikprops.values.password}
             />
             <Button
-              title="submit"
-              color="maroon"
+              style={styles.button}
+              mode="contained"
               onPress={formikprops.handleSubmit}
-            />
+            >
+              Register
+            </Button>
           </View>
         )}
       </Formik>
@@ -51,12 +55,36 @@ const RegisterForm = () => {
 };
 
 const styles = StyleSheet.create({
-  input: {
-    borderWidth: 1,
-    borderColor: "#ddd",
-    padding: 10,
-    fontSize: 18,
-    borderRadius: 6,
+  container: {
+    flex: 1,
+    backgroundColor: "#003f5c",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    width: 300,
+    backgroundColor: "#465881",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 40,
+    color: "#fb5b5a",
+    marginBottom: 40,
+  },
+  button: {
+    width: 300,
+    backgroundColor: "#fb5b5a",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 15,
+    marginBottom: 10,
   },
 });
 
